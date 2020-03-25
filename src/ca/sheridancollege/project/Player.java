8f535bd17dd1657af3a1bdfa6ad57c03722a7da7
kiranpki <kiranpki@sheridancollege.ca>
2020-03-25 19:53:58 -0400

    Blackjack processing

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -12,7 +12,7 @@ package ca.sheridancollege.project;
 public abstract class Player 
 {
     private String playerID; //The Player iD gives the Identity to players
-    
+    private String GameName; //The name of the game
     /**
      * A constructor that allows you to set the player's unique ID
      */
@@ -21,6 +21,11 @@ public abstract class Player
         playerID= name;
     }
    //getters and setters for the PlayerID
+
+    public String getGameName() {
+        return GameName;
+    }
+    
     public String getPlayerID() 
     {
         return playerID;
